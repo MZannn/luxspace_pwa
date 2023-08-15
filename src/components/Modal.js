61db617a57241eb2e086b29dfa54eeaf414149b3
MZannn <mfauzan1098@gmail.com>
2023-08-15 23:00:53 +0700

    update className old version to new version tailwind css

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,14 +5,13 @@ function Modal({handleShowModal}) {
         className="fixed inset-0 z-40 flex items-center justify-center w-100  min-h-screen"
         onClick={handleShowModal}
       >
-        <div className="fixed inset-0 bg-black opacity-35"></div>
+        <div className="fixed inset-0 bg-black opacity-30"></div>
         <div className="bg-white p-0 md:p-6 z-10">
-          <div className="w-screen pb-56 md:w-88 md:pb-56 relative z-50">
-            <div className="absolute w-full h-full">
+          <div className="w-screen max-w-md mx-auto relative z-50">
+            <div className="aspect-w-16 aspect-h-9">
               <iframe
+                className="w-full aspect-video"
                 title="Video"
-                width="100%"
-                height="100%"
                 src="https://www.youtube.com/embed/3h0_v1cdUIA"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
